4c30ab93484fed912a6242eb971bae8cc0019342
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-24 15:03:46 +0100

    functionnal building windows

--- app/src/main/java/com/faircorp/WindowsActivity.kt
+++ app/src/main/java/com/faircorp/WindowsActivity.kt
@@ -2,10 +2,28 @@ package com.faircorp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.faircorp.model.WindowAdapter
+import com.faircorp.model.WindowService
 
 class WindowsActivity : BasicActivity() {
+
+    val windowService = WindowService() // (1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_windows)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list_windows) // (2)
+        val adapter = WindowAdapter() // (3)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        adapter.update(windowService.findAll()) // (4)
     }
 }
