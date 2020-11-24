e256de007e963f0b5a5cdf56246564d33c2ff976
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-24 12:09:47 +0100

    add window activity

--- app/src/main/java/com/faircorp/MainActivity.kt
+++ app/src/main/java/com/faircorp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.faircorp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
-import android.widget.Toast
+
+const val WINDOW_NAME_PARAM = "com.faircorp.windowname.attribute"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +16,12 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the button */
     fun openWindow(view: View) {
-        // Extract value filled in editext identified with txt_window_name id
         val windowName = findViewById<EditText>(R.id.txt_window_name).text.toString()
-        // Display a message
-        Toast.makeText(this, "You choose $windowName", Toast.LENGTH_LONG).show()
+
+        // Do something in response to button
+        val intent = Intent(this, WindowActivity::class.java).apply {
+            putExtra(WINDOW_NAME_PARAM, windowName)
+        }
+        startActivity(intent)
     }
 }
