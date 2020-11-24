24a7c835d41eed743e7243fd5a0303d0c0d7493f
Alexandre <alexandre.verdet@etu.emse.fr>
2020-11-24 12:22:07 +0100

    fix window activity and add return option

--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -4,16 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-const val WINDOW_NAME_PARAM = "com.faircorp.windowname.attribute"
 
-class MainActivity : AppCompatActivity() {
+class WindowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val param = intent.getStringExtra(WINDOW_NAME_PARAM)
-        val windowName = findViewById<TextView>(R.id.txt_window_name)
+        val windowName = findViewById<TextView>(R.id.txt_window_name2)
         windowName.text = param
     }
 }
